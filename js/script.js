77c8c56a5f06bd4db5b7e63fbdc64428663e8823
Ayaan <63299939+IamAyaanSk@users.noreply.github.com>
2023-07-05 17:28:43 +0530

    Added functionality : movies, shows,  spinner

--- js/script.js
+++ js/script.js
@@ -1,7 +1,97 @@
-const api = "621cfb335fa28cf9d3189a4e71af2ef0";
 const global = {
   currentPage: window.location.pathname,
 };
+
+/*
+    HOME PAGE
+*/
+
+//List Popular Movies
+async function listPopularMovies() {
+  const results = await fetchData("movie/popular");
+
+  results.forEach((movie) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+    <a href="movie-details.html?${movie.id}">
+    ${
+      movie.poster_path
+        ? `<img
+            src="https://image.tmdb.org/t/p/w500/${movie.poster_path}"
+            class="card-img-top"
+            alt="${movie.title}"
+          />`
+        : `<img
+            src="images/no-image.jpg"
+            class="card-img-top"
+            alt="Movie Title"
+        />`
+    }
+    </a>
+    <div class="card-body">
+        <h5 class="card-title">${movie.title}</h5>
+        <p class="card-text">
+        <small class="text-muted">Release: ${movie.release_date}</small>
+        </p>
+    </div>
+    `;
+    document.querySelector("#popular-movies").appendChild(div);
+  });
+}
+
+/*
+   TV SHOWS
+*/
+//List Populat TV Shows
+async function listPopularShows() {
+  const results = await fetchData("tv/popular");
+
+  results.forEach((show) => {
+    const div = document.createElement("div");
+    div.classList.add("card");
+    div.innerHTML = `
+      <a href="movie-details.html?${show.id}">
+      ${
+        show.poster_path
+          ? `<img
+              src="https://image.tmdb.org/t/p/w500/${show.poster_path}"
+              class="card-img-top"
+              alt="${show.name}"
+            />`
+          : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="Movie Title"
+          />`
+      }
+      </a>
+      <div class="card-body">
+          <h5 class="card-title">${show.name}</h5>
+          <p class="card-text">
+          <small class="text-muted">Release: ${show.first_air_date}</small>
+          </p>
+      </div>
+      `;
+    document.querySelector("#popular-shows").appendChild(div);
+  });
+}
+
+/*
+  GENERAL FUNCTIONS
+*/
+
+//Fetch data via endpoint
+async function fetchData(endpoint) {
+  const API_KEY = "621cfb335fa28cf9d3189a4e71af2ef0";
+  const API_URL = "https://api.themoviedb.org/3/";
+  showSpinner();
+
+  const res = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}`);
+  const { results } = await res.json();
+  hideeSpinner();
+  return results;
+}
 
 //Focus navbar elements
 function highlightActive() {
@@ -13,16 +103,27 @@ function highlightActive() {
   });
 }
 
+//Show spinner
+function showSpinner() {
+  document.querySelector(".spinner").classList.add("show");
+}
+
+//Hide Spinner
+
+function hideeSpinner() {
+  document.querySelector(".spinner").classList.remove("show");
+}
+
 //Initialize function
 function init() {
   switch (global.currentPage) {
     case "/":
     case "/index.html":
-      console.log("Home");
+      listPopularMovies();
       break;
 
     case "/shows.html":
-      console.log("Shows");
+      listPopularShows();
       break;
 
     case "/movie-details.html":
